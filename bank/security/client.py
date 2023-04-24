3ee13f1debfd4b5dc594862cb922c48cbff77b52
DevMaycon <Dev.Maycon08@gmail.com>
2023-04-23 23:00:36 -0300

    Added Projects.

---
+++ bank/security/client.py
@@ -0,0 +1,17 @@
+import base64
+
+class AccountBank:
+    phone: int
+    name: str
+    password: str
+    
+    def __init__(self, id: int, accountData: dict) -> None:
+        self.id = id
+        self.name = accountData["name"]
+        self.password = accountData["password"]
+        while True:
+            text = input(f"Olá{self.name}, oque ira fazer? :\n")
+            try:
+                getattr(self, text)
+            except:
+                pass
